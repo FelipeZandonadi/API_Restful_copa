0de053ac433d2fa34209e09732157048033f83cf
Felipe Zandonadi <zhifelipe@gmail.com>
2024-05-11 16:36:39 -0300

    add data base connection

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 import app from './src/app.js'
-import connection from './infra/connection_db.js'
 
-connection.connect((error) => {
-    if (error) {
-        console.error(error)
-    } else {
-        console.log('Conexão com o Data base feita com sucesso!')
-        // escutar a porta 8080
-        app.listen(8080, ()=>{
-        console.log(`Servidor rodando no endereço http://localhost:8080`)
-        })
-    }
-})
+app.listen(8080, ()=>{
+    console.log(`Servidor rodando no endereço http://localhost:8080`)
+    })
--- src/app.js
+++ src/app.js
@@ -1,41 +1,11 @@
 import express from 'express'
 
+import conexao from '../infra/connection_db.js'
+
 const app = express()
 
 // indicar para express ler body (res) como json
 app.use(express.json())
-
-// mock de dados
-const selecoes = [
-    {
-        id: 1, 
-        selecao: 'Brasil', 
-        group: 'G'
-    },
-    {
-        id: 2, 
-        selecao: 'Suíça', 
-        group: 'G'
-    },
-    {
-        id: 3, 
-        selecao: 'Camarões',
-        group: 'G'
-    },
-    {
-        id: 4, 
-        selecao: 'Sérvia', 
-        group: 'G'
-    }
-]
-
-// Funções de busca
-function buscaPorId(id) {
-    return selecoes.filter( selecao => selecao.id === id) // return um array com apenas UM objeto dentro
-}
-function buscarIndexSelecao(id) {
-    return selecoes.findIndex(selecao => selecao.id === id)
-}
 
 // ------------------------------------------------------------------------------------------------------------------------------
 // C.R.U.D -> Create, Read, Update, Delete
@@ -44,41 +14,66 @@ function buscarIndexSelecao(id) {
 // Create
 
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso!')
+    const selecao = req.body
+    const sql = 'INSERT INTO selecoes SET ?'
+    conexao.query(sql, selecao, (error, result) => {
+        if (error) {
+            res.status(400).json({ 'Alert': 'Dados não localizados', 'error': error })
+        } else {
+            res.status(201).json(result)
+        }
+    })
 })
 
 // Read
-// Lê rota raiz (padrão)
-app.get('/', (req, res) => {
-    res.send('<a href="http://localhost:8080/selecoes">selecoes</a>')
-})
 // Lê o mock das selecoes
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes)
+    const sql = 'SELECT * FROM selecoes;'
+    conexao.query(sql, (error, result) => {
+        if (error) {
+            res.status(404).json({ 'Alert': 'Dados não localizados', 'error': error })
+        } else {
+            res.status(200).json(result)
+        }
+    })
 })
 // Lê o mock das selecoes por id
 app.get('/selecoes/:id', (req, res) => {
-    if (buscaPorId(Number(req.params.id))[0]) {
-        res.json(buscaPorId(Number(req.params.id))[0])
-    } else {
-        res.send(`Seleção de Id ${req.params.id} disponível --> <a href="https://www.youtube.com/watch?v=GtL1huin9EE">aqui</a> <--`)
-    }
+    const id = req.params.id
+    const sql = 'SELECT * FROM selecoes WHERE id=?'
+    conexao.query(sql, id, (error, result) => {
+        if (error) {
+            res.status(404).json({ 'Alert': 'Dados não localizados', 'error': error })
+        } else {
+            res.status(200).json(result[0])
+        }
+    })
 })
 
 // Update
 app.put('/selecoes/:id', (req, res) => {
-    // let selecaoDeleted = buscaPorId(Number(req.params.id))[0].selecao
-    selecoes[buscarIndexSelecao(Number(req.params.id))].selecao = req.body.selecao
-    selecoes[buscarIndexSelecao(Number(req.params.id))].group = req.body.group
-    res.json(selecoes)
+    const selecao = req.body
+    const sql = 'UPDATE selecoes SET ? WHERE id=?'
+    conexao.query(sql, [selecao, req.params.id], (error, result) => {
+        if (error) {
+            res.status(404).json({ 'Alert': 'Dados não localizados', 'error': error })
+        } else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 // Delete
 app.delete('/selecoes/:id', (req, res) => {
-    let selecaoDeleted = buscaPorId(Number(req.params.id))[0].selecao
-    selecoes.splice(buscarIndexSelecao(Number(req.params.id)), 1)
-    res.send(`Seleção ${selecaoDeleted} apagada com sucesso!`)
+    const id = req.params.id
+    const sql = 'DELETE FROM selecoes WHERE id=?'
+    conexao.query(sql, id, (error, result) => {
+        if (error) {
+            res.status(404).json({ 'Alert': 'Dados não localizados', 'error': error })
+        } else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 export default app
